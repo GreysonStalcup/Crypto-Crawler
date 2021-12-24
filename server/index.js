ad2c2425e33985ce5cff7a670b4740c5ba154ec6
GreysonStalcup <g_stalcup@hotmail.com>
2021-12-23 17:52:42 -0800

    changes to server

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.get('/', async (req, res) =>{
     const dataStuff = await getCoinPrice(coin);
     //let priceOfCoin = data.coin.usd;
     // console.log(priceOfCoin);
-    res.send(dataStuff);
-    let coinName = coin.toString();
-    console.log(dataStuff.data);
+    res.send(dataStuff)
+    
+    // dataStuff.data[coin].usd
     
 
     
@@ -38,10 +38,15 @@ async function getCoinPrice(coin){
         ids: coin,
         vs_currencies: ['usd'],
     });
-    let coinName = coin.toString();
-    console.log(coinName)
-    
-    return data;
+    //let coinName = coin.toString();
+    //console.log(coinName)
+    console.log(data.data[coin].usd)
+    const info = {
+        coin: coin,
+        usd: data.data[coin].usd
+    };
+    return info;
+    console.log(info) 
     
 };
 
